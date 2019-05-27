255407e4453e03401fc98306cf33adb77386455d
vnikita0109 <vnikita793@gmail.com>
2019-05-27 10:11:14 +0530

    Constructor injection

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -20,16 +20,22 @@ public class Main {
         System.out.println(movie0.getActor().getName()+ " acted in: " + movie0.getMovieName());
 */
         ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
-        Movie movie=context.getBean("movie",Movie.class);
-        System.out.println(movie.getMovieName()+" has actor "+movie.getActor().getName());
-
-       /* Movie movie1=context.getBean("movie",Movie.class);
-        Movie movie2=context.getBean("movie",Movie.class);
-        System.out.println(movie1==movie2);
-       Movie movie1=context.getBean("Doctor",Movie.class);
+        Movie movie1=context.getBean("Doctor",Movie.class);
         Movie movie2=context.getBean("Highkick",Movie.class);
         System.out.println(movie1==movie2);
 
+        //ApplicationContext context1=new ClassPathXmlApplicationContext("beans.xml");
+
+        System.out.println("f movie names are diff");
+        Movie movieBean1=context.getBean("K2",Movie.class);
+        Movie movieBean2=context.getBean("Killer",Movie.class);
+        System.out.println(movieBean1);
+        System.out.println(movieBean2);
+       /*Movie movie1=context.getBean("movie",Movie.class);
+        Movie movie2=context.getBean("movie",Movie.class);
+        System.out.println(movie1==movie2);*/
+
+/*
        ClassPathXmlApplicationContext obj=(ClassPathXmlApplicationContext)context;
        obj.destroy();*/
     }
